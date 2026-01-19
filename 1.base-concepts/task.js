eb9f595195901ef7238c16b872b3b3d1d1fe2659
Leyla <lagvilava94@gmail.com>
2026-01-19 14:03:44 +0300

    1st homework formatted

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,26 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
 
-  const d = b **2 - 4 * a * c;
-  if(d < 0) {
-      return [];
-    } else if(d === 0) {
-      const root = -b / (2 * a);
-      return [root]
-    }
-    else {
-      const sqrtD = Math.sqrt(d);
-      const root1 = (-b + Math.sqrt(d) ) / (2 * a);
-      const root2 = (-b - Math.sqrt(d) ) / (2 * a);
-      return [root1, root2];
+	return arr;
 
-    }
+	const d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		return [];
+	} else if (d === 0) {
+		const root = -b / (2 * a);
+		return [root]
+	} else {
+		const sqrtD = Math.sqrt(d);
+		const root1 = (-b + Math.sqrt(d)) / (2 * a);
+		const root2 = (-b - Math.sqrt(d)) / (2 * a);
+		return [root1, root2];
+
+	}
 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
