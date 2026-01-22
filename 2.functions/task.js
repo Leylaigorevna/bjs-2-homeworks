ddc627eeca04ad1348f4f12ee07a016f54dadf45
Leyla <lagvilava94@gmail.com>
2026-01-22 13:29:59 +0300

    1st task added

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,20 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    if(num > max) {
+      max = num;
+    }
+    if(num < min) {
+      min = num;
+    }
+   sum += num;
+  }
 
-  return { min: min, max: max, avg: avg };
+  const avg = +(sum / arr.length).toFixed(2);
+  return { min, max, avg };
 }
 
 function summElementsWorker(...arr) {
