f5dbc98d4791a60f6b31ef10e0e5247a672fe479
Leyla <lagvilava94@gmail.com>
2026-01-22 14:24:47 +0300

    3rd task added

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -70,5 +70,14 @@ function averageEvenElementsWorker(...arr) {
 }
 
 function makeWork (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
 
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const result = func(...arrOfArr[i]);
+
+    if (result > maxWorkerResult)  {
+      maxWorkerResult = result;
+    }
+  }
+  return maxWorkerResult;
 }
