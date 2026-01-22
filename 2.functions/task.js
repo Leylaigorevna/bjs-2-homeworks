7a355800af09acbf6eecb379ec586c471243f2c5
Leyla <lagvilava94@gmail.com>
2026-01-22 14:22:17 +0300

    2nd task added

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -18,19 +18,55 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  let sum = 0;
+  for (let i= 0;i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) return 0;
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+
+    return max - min;
+  }
 
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) return 0;
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
 
+  for (let i= 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i= 0;i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+  if (countEven === 0) return 0;
+  return +(sumEven / countEven).toFixed(2);
 }
 
 function makeWork (arrOfArr, func) {
