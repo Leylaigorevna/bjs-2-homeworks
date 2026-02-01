c4518dcbba5440a9ea53bd985b73afa9c86c8117
Leyla <lagvilava94@gmail.com>
2026-02-01 13:23:08 +0300

    2nd task added

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -13,3 +13,45 @@ function validateCount(value) {
     return error;
   }
 }
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (
+      a + b <= c ||
+      a + c <= b ||
+      b + c <= a
+    ) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const s = this.perimeter / 2;
+    const areaCalculated = Math.sqrt(
+      s * (s - this.a) * (s - this.b) * (s - this.c)
+    );
+    return areaCalculated.toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      },
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      }
+    };
+  }
+}
